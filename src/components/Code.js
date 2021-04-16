19edb84afa0beb4c6fef9aa992644bf8acc3d37f
Greg <gpearson100@gmail.com>
2021-04-16 11:10:37 -0500

    fixing import/no-anonymous-default-export

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,12 +1,16 @@
-
-import React, { useState } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Card, Button, Tooltip, OverlayTrigger } from '@themesberg/react-bootstrap';
-import Highlight, { Prism } from 'prism-react-renderer';
+import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import {
+  Card,
+  Button,
+  Tooltip,
+  OverlayTrigger,
+} from "@themesberg/react-bootstrap";
+import Highlight, { Prism } from "prism-react-renderer";
 
 import themeStyle from "../assets/syntax-themes/ghcolors.json";
 
-export default (props) => {
+const Code = (props) => {
   const { code = "", language = "jsx" } = props;
   const [copied, setCopied] = useState(false);
 
@@ -15,26 +19,37 @@ export default (props) => {
     setTimeout(() => setCopied(false), 2000);
   };
 
-  const CodeStyling = ({ className, style, tokens, getLineProps, getTokenProps }) => (
+  const CodeStyling = ({
+    className,
+    style,
+    tokens,
+    getLineProps,
+    getTokenProps,
+  }) => (
     <Card className="position-relative pe-8 mb-2">
       <Card.Body>
         <pre className={className} style={style}>
           {tokens.map((line, i) => (
             <div {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => <span {...getTokenProps({ token, key })} />)}
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
             </div>
           ))}
         </pre>
 
-        {copied ? <span className="text-success copy-code-text">Copied</span> : null}
+        {copied ? (
+          <span className="text-success copy-code-text">Copied</span>
+        ) : null}
 
         <OverlayTrigger
-          trigger={['hover', 'focus']}
+          trigger={["hover", "focus"]}
           placement="top"
-          overlay={<Tooltip>Copy to clipboard</Tooltip>}
-        >
+          overlay={<Tooltip>Copy to clipboard</Tooltip>}>
           <CopyToClipboard text={code} onCopy={handleCopy}>
-            <Button size="sm" variant="primary" className="copy-code-button">Copy</Button>
+            <Button size="sm" variant="primary" className="copy-code-button">
+              Copy
+            </Button>
           </CopyToClipboard>
         </OverlayTrigger>
       </Card.Body>
@@ -48,3 +63,4 @@ export default (props) => {
   );
 };
 
+export default Code;
--- src/pages/documentation/DocsBuild.js
+++ src/pages/documentation/DocsBuild.js
@@ -1,8 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import { Row, Col, Card, Container } from "@themesberg/react-bootstrap";
 
-import React from 'react';
-import { Row, Col, Card, Container } from '@themesberg/react-bootstrap';
-
-import { CommandsTable } from '../../components/Tables';
+import { CommandsTable } from "../../components/Tables";
 
 export default () => (
   <Container className="px-0">
@@ -11,8 +11,14 @@ export default () => (
         <Card>
           <Card.Body>
             <article>
-              <h1 className="h2" id="build-tools">Build tools </h1>
-              <p className="fs-5 fw-light">Learn the commands that will help you work with Volt React Dashboard and the commands that can build the final production version of your app.</p>
+              <h1 className="h2" id="build-tools">
+                Build tools{" "}
+              </h1>
+              <p className="fs-5 fw-light">
+                Learn the commands that will help you work with Volt React
+                Dashboard and the commands that can build the final production
+                version of your app.
+              </p>
 
               <p className="fs-5 fw-bold">Available commands</p>
               <CommandsTable />
